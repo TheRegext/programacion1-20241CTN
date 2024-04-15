c8b64adf60319b9afe0a4d7dc336ce0b0a458f88
ArielTapia <arieltapia42@gmail.com>
2024-04-14 21:46:24 -0300

    Actualizacion

--- Semana4/ejemplo5.cpp
+++ Semana4/ejemplo5.cpp
@@ -8,30 +8,16 @@
 ///1- EL PROMEDIO DE NOTAS
 ///2- EL PROMEDIO DE NOTAS DE LOS APROBADOS (>=6)
 
+
+
+///  ************ TAREA PENDIENTE PARA PROXIMA CLASE ************ 
+
 #include <iostream>
 
 using namespace std;
 
 int main()
 {
-
-    int i, num;
-    int cont=0;
-    for(i = 1; i <= 4; i++) {
-        cout << "Ingrese #"<< i <<": ";
-        cin >> num;
-
-        if(num%2==0)
-        {
-            cont++;
-        }
-    }
-    if(cont==4)
-        cout << "SON TODOS PARES"<< endl;
-    else if(cont > 0)
-        cout << "HAY AL MENOS UN PAR"<< endl;
-    else
-        cout << "NO HAY PARES" << endl;
 
     return 0;
 }
